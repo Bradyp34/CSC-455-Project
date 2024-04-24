bd37468f8f474e41d35c1b5f2d5c7ff6238feffb
Asher Griess <Asher.Griess@trojans.dsu.edu>
2024-04-24 15:15:02 -0500

    Create Basic Start UI

--- CSC 455 Project/Form1.Designer.cs
+++ CSC 455 Project/Form1.Designer.cs
@@ -16,26 +16,88 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent () {
-			SuspendLayout();
-			// 
-			// Form1
-			// 
-			AutoScaleDimensions = new SizeF(10F, 25F);
-			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
-			Name = "Form1";
-			Text = "Form1";
-			Load += Form1_Load;
-			ResumeLayout(false);
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(12, 29);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(211, 190);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(65, 275);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(65, 252);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Workout Name";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(219, 275);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Create";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(74, 6);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Workouts";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(640, 360);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(listView1);
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListView listView1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+    }
 }
--- CSC 455 Project/Form1.cs
+++ CSC 455 Project/Form1.cs
@@ -1,11 +1,19 @@
 namespace CSC_455_Project {
-	public partial class Form1 : Form {
-		public Form1 () {
-			InitializeComponent();
-		}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-		private void Form1_Load (object sender, EventArgs e) {
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
-		}
-	}
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
