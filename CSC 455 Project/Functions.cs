28cf4cc3ebc7cf84c3129016609d363ff64c1e37
bradyp34 <brady.petterson@gmail.com>
2024-04-29 23:24:26 -0500

    created tests for all functions on functions page

--- CSC 455 Project/Functions.cs
+++ CSC 455 Project/Functions.cs
@@ -6,87 +6,71 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CSC_455_Project
-{
-    public static class Functions
-    {
+namespace CSC_455_Project {
+	public static class Functions {
 
-        // Search Functions
-        public static Workout SearchForWorkout(HashSet<Workout> workouts, string name) 
-        {
-            return workouts.FirstOrDefault(i => i.name == name);
-        }
+		// Search Functions
+		public static Workout SearchForWorkout (HashSet<Workout> workouts, string name) {
+			return workouts.FirstOrDefault(i => i.name == name);
+		}
 
-        public static Exercise SearchForExercise(HashSet<Exercise> exercises, string name)
-        {
-            return exercises.FirstOrDefault(i => i.name == name);
-        }
+		public static Exercise SearchForExercise (HashSet<Exercise> exercises, string name) {
+			return exercises.FirstOrDefault(i => i.name == name);
+		}
 
-        public static void SaveWorkouts(HashSet<Workout> workouts)
-        {
-            string jsonString = JsonSerializer.Serialize(workouts);
-            string fileName = "Workouts.json";
-            File.WriteAllText(fileName, jsonString);
-        }
+		public static void SaveWorkouts (HashSet<Workout> workouts) {
+			string jsonString = JsonSerializer.Serialize(workouts);
+			string fileName = "Workouts.json";
+			File.WriteAllText(fileName, jsonString);
+		}
 
-        public static HashSet<Workout> LoadWorkouts() 
-        {
-            // Load workouts
-            try
-            {
+		public static HashSet<Workout> LoadWorkouts () {
+			// Load workouts
+			try {
 
-                string fileName = "Workouts.json";
-                string jsonString = File.ReadAllText(fileName);
-                return JsonSerializer.Deserialize<HashSet<Workout>>(jsonString)!;
-                
-            }
-            catch (Exception ex)
-            {
-                return new HashSet<Workout>();
-            }
-        }
+				string fileName = "Workouts.json";
+				string jsonString = File.ReadAllText(fileName);
+				return JsonSerializer.Deserialize<HashSet<Workout>>(jsonString)!;
 
-        public static void SaveWorkoutDates(HashSet<WorkoutDay> workoutDays)
-        {
-            string fileName = "WorkoutDays.json";
-            string jsonString = JsonSerializer.Serialize(workoutDays);
-            File.WriteAllText(fileName, jsonString);
-        }
+			}
+			catch (Exception ex) {
+				return new HashSet<Workout>();
+			}
+		}
 
-        public static HashSet<WorkoutDay> LoadWorkoutDates()
-        {
-            try
-            {
+		public static void SaveWorkoutDates (HashSet<WorkoutDay> workoutDays) {
+			string fileName = "WorkoutDays.json";
+			string jsonString = JsonSerializer.Serialize(workoutDays);
+			File.WriteAllText(fileName, jsonString);
+		}
 
-                string fileName = "WorkoutDays.json";
-                string jsonString = File.ReadAllText(fileName);
-                return JsonSerializer.Deserialize<HashSet<WorkoutDay>>(jsonString)!;
-            }
-            catch (Exception ex)
-            {
-                return new HashSet<WorkoutDay>();
-            }
-        }
+		public static HashSet<WorkoutDay> LoadWorkoutDates () {
+			try {
 
-        public static WorkoutDay CheckForWorkoutDay(HashSet<WorkoutDay> workoutDays, string date)
-        {
-            // Check if already exists
-            WorkoutDay exist = workoutDays.FirstOrDefault(x => x.day == DateOnly.Parse(date));
-            WorkoutDay workday;
-            if (exist != null)
-            {
-                workday = exist;
-            }
-            else
-            {
-                workday = new WorkoutDay(DateOnly.Parse(date));
-            }
-            return workday;
-        }
+				string fileName = "WorkoutDays.json";
+				string jsonString = File.ReadAllText(fileName);
+				return JsonSerializer.Deserialize<HashSet<WorkoutDay>>(jsonString)!;
+			}
+			catch (Exception ex) {
+				return new HashSet<WorkoutDay>();
+			}
+		}
 
-        public static void RemoveExercise(HashSet<Exercise> exercises, string name)
-        {
-            exercises.Remove(exercises.FirstOrDefault(x => x.name == name));
-        }
+		public static WorkoutDay CheckForWorkoutDay (HashSet<WorkoutDay> workoutDays, string date) {
+			// Check if already exists
+			WorkoutDay exist = workoutDays.FirstOrDefault(x => x.day == DateOnly.Parse(date));
+			WorkoutDay workday;
+			if (exist != null) {
+				workday = exist;
+			}
+			else {
+				workday = new WorkoutDay(DateOnly.Parse(date));
+			}
+			return workday;
+		}
+
+		public static void RemoveExercise (HashSet<Exercise> exercises, string name) {
+			exercises.Remove(exercises.FirstOrDefault(x => x.name == name));
+		}
 	}
 }
--- CSC 455 ProjectTests/FunctionsTests.cs
+++ CSC 455 ProjectTests/FunctionsTests.cs
@@ -72,5 +72,189 @@ namespace CSC_455_Project.Tests {
 			// Add your assertions here
 			Assert.IsTrue(result == null);
 		}
+
+		[TestMethod()]
+		public void SaveWorkoutsTest () {
+			// Arrange
+			var workout1 = new Workout("Workout 1");
+			var workout2 = new Workout("Workout 2");
+
+			var workouts = new HashSet<Workout> { workout1, workout2 };
+
+			// Act
+			Functions.SaveWorkouts(workouts);
+
+			// Assert
+			// Add your assertions here
+			Assert.IsTrue(File.Exists("Workouts.json"));
+		}
+
+		[TestMethod()]
+		public void LoadWorkoutsTest () {
+			// Arrange
+			var workout1 = new Workout("Workout 1");
+			var workout2 = new Workout("Workout 2");
+
+			var workouts = new HashSet<Workout> { workout1, workout2 };
+
+			Functions.SaveWorkouts(workouts);
+
+			// Act
+			var result = Functions.LoadWorkouts();
+
+			// Assert
+			// Add your assertions here
+			Assert.IsTrue(result.Count == 2);
+		}
+
+		[TestMethod()]
+		public void SaveWorkoutDatesTest () {
+			// Arrange
+			var workoutDay1 = new WorkoutDay(DateOnly.Parse("2022-01-01"));
+			var workoutDay2 = new WorkoutDay(DateOnly.Parse("2022-01-02"));
+
+			var workoutDays = new HashSet<WorkoutDay> { workoutDay1, workoutDay2 };
+
+			// Act
+			Functions.SaveWorkoutDates(workoutDays);
+
+			// Assert
+			// Add your assertions here
+			Assert.IsTrue(File.Exists("WorkoutDays.json"));
+		}
+
+		[TestMethod()]
+		public void LoadWorkoutDatesTest () {
+			// Arrange
+			var workoutDay1 = new WorkoutDay(DateOnly.Parse("2022-01-01"));
+			var workoutDay2 = new WorkoutDay(DateOnly.Parse("2022-01-02"));
+
+			var workoutDays = new HashSet<WorkoutDay> { workoutDay1, workoutDay2 };
+
+			Functions.SaveWorkoutDates(workoutDays);
+
+			// Act
+			var result = Functions.LoadWorkoutDates();
+
+			// Assert
+			// Add your assertions here
+			Assert.IsTrue(result.Count == 2);
+		}
+
+		[TestMethod()]
+		public void CheckForWorkoutDayTest () {
+			// Arrange
+			var workoutDay1 = new WorkoutDay(DateOnly.Parse("2022-01-01"));
+			var workoutDay2 = new WorkoutDay(DateOnly.Parse("2022-01-02"));
+
+			var workoutDays = new HashSet<WorkoutDay> { workoutDay1, workoutDay2 };
+
+			// Act
+			var result = Functions.CheckForWorkoutDay(workoutDays, "2022-01-01");
+
+			// Assert
+			// Add your assertions here
+			Assert.IsTrue(result == workoutDay1);
+		}
+
+		[TestMethod()]
+		public void CheckForWorkoutDayFailTest () {
+			// Arrange
+			var workoutDay1 = new WorkoutDay(DateOnly.Parse("2022-01-01"));
+			var workoutDay2 = new WorkoutDay(DateOnly.Parse("2022-01-02"));
+
+			var workoutDays = new HashSet<WorkoutDay> { workoutDay1, workoutDay2 };
+
+			// Act
+			var result = Functions.CheckForWorkoutDay(workoutDays, "2022-01-03");
+
+			// Assert
+			// Add your assertions here
+			Assert.IsTrue(result == null);
+		}
+
+		[TestMethod()]
+		public void CheckForWorkoutDayNewTest () {
+			// Arrange
+			var workoutDay1 = new WorkoutDay(DateOnly.Parse("2022-01-01"));
+			var workoutDay2 = new WorkoutDay(DateOnly.Parse("2022-01-02"));
+
+			var workoutDays = new HashSet<WorkoutDay> { workoutDay1, workoutDay2 };
+
+			// Act
+			var result = Functions.CheckForWorkoutDay(workoutDays, "2022-01-03");
+
+			// Assert
+			// Add your assertions here
+			Assert.IsTrue(result.day == DateOnly.Parse("2022-01-03"));
+		}
+
+		[TestMethod]
+		public void CheckForWorkoutDay_ExistingDate_ReturnsExistingWorkoutDay () {
+			// Arrange
+			var workoutDays = new HashSet<WorkoutDay>
+			{
+		new WorkoutDay(DateOnly.Parse("2022-01-01"))
+	};
+			var date = "2022-01-01";
+
+			// Act
+			var result = Functions.CheckForWorkoutDay(workoutDays, date);
+
+			// Assert
+			Assert.AreEqual(DateOnly.Parse(date), result.day);
+		}
+
+		[TestMethod]
+		public void CheckForWorkoutDay_NonExistingDate_ReturnsNewWorkoutDay () {
+			// Arrange
+			var workoutDays = new HashSet<WorkoutDay>
+			{
+		new WorkoutDay(DateOnly.Parse("2022-01-01"))
+	};
+			var date = "2022-02-02";
+
+			// Act
+			var result = Functions.CheckForWorkoutDay(workoutDays, date);
+
+			// Assert
+			Assert.AreEqual(DateOnly.Parse(date), result.day);
+		}
+
+		[TestMethod]
+		public void RemoveExerciseTest() {
+			// Arrange
+			var exercise1 = new Exercise("Exercise 1");
+			var exercise2 = new Exercise("Exercise 2");
+
+			var exercises = new HashSet<Exercise> { exercise1, exercise2 };
+
+			// Act
+			Functions.RemoveExercise(exercises, "Exercise 1");
+
+			// Assert
+			// Add your assertions here
+			Assert.IsTrue(exercises.Count == 1);
+			Assert.IsTrue(exercises.Contains(exercise2));
+			Assert.IsFalse(exercises.Contains(exercise1));
+		}
+
+		[TestMethod]
+		public void RemoveExerciseTestFail() {
+			// Arrange
+			var exercise1 = new Exercise("Exercise 1");
+			var exercise2 = new Exercise("Exercise 2");
+
+			var exercises = new HashSet<Exercise> { exercise1, exercise2 };
+
+			// Act
+			Functions.RemoveExercise(exercises, "Exercise 3");
+
+			// Assert
+			// Add your assertions here
+			Assert.IsTrue(exercises.Count == 2);
+			Assert.IsTrue(exercises.Contains(exercise1));
+			Assert.IsTrue(exercises.Contains(exercise2));
+		}
 	}
 }
