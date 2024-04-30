c2b36acc73bfbb3a3fff4232798bca199a281e01
bradyp34 <brady.petterson@gmail.com>
2024-04-30 00:11:45 -0500

    added Moq and NUnit for testing, refactored editworkout.cs page

--- CSC 455 Project/Workout/EditWorkout.cs
+++ CSC 455 Project/Workout/EditWorkout.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -11,42 +10,46 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
-namespace CSC_455_Project
-{
+namespace CSC_455_Project {
+	public interface IExerciseFunctions {
+		Exercise SearchForExercise (List<Exercise> exercises, string name);
+		void RemoveExercise (List<Exercise> exercises, string name);
+	}
+
 	public partial class EditWorkout : Form {
+		private readonly IExerciseFunctions _exerciseFunctions;
+
+		public EditWorkout (IExerciseFunctions exerciseFunctions) {
+			_exerciseFunctions = exerciseFunctions;
+		}
 
 		public void button1_Click (object sender, EventArgs e) {
-
 			Exercise exercise = new Exercise(NameInput.Text);
 			workout.addExercise(exercise);
 
-			var box = new NewExercise(exercise);
-			box.ShowDialog();
-			// Refresh List
+			ShowNewExerciseDialog(exercise);
 			RefreshList();
 		}
 
 		public void button2_Click (object sender, EventArgs e) {
-			Exercise exercise = Functions.SearchForExercise(workout.exercises, listBox1.SelectedItem.ToString());
+			Exercise exercise = _exerciseFunctions.SearchForExercise(workout.exercises.ToList(), listBox1.SelectedItem.ToString());
 			if (exercise != null) {
-				var box = new NewExercise(exercise);
-				box.ShowDialog();
+				ShowNewExerciseDialog(exercise);
 			}
 		}
 
 		public void DeleteButton_Click (object sender, EventArgs e) {
 			var selectedItem = listBox1.SelectedItem;
 
-
-			// Make sure item is selected
 			if (selectedItem != null) {
-				Functions.RemoveExercise(workout.exercises, selectedItem.ToString());
+				_exerciseFunctions.RemoveExercise(workout.exercises.ToList(), selectedItem.ToString());
+				RefreshList();
 			}
-			RefreshList();
 		}
 
-		public void EditWorkout_Load (object sender, EventArgs e) {
-
+		private void ShowNewExerciseDialog (Exercise exercise) {
+			var box = new NewExercise(exercise);
+			box.ShowDialog();
 		}
 	}
 }
--- CSC 455 ProjectTests/Workout/EditWorkoutTests.cs
+++ CSC 455 ProjectTests/Workout/EditWorkoutTests.cs
@@ -5,10 +5,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
 
-namespace CSC_455_Project.Tests {
-	[TestClass()]
+namespace CSC_455_Project_Tests {
+	[TestFixture]
 	public class EditWorkoutTests {
-		
+		private Mock<IExerciseFunctions> _mockExerciseFunctions;
+		private EditWorkout _editWorkout;
+
+		[SetUp]
+		public void SetUp () {
+			_mockExerciseFunctions = new Mock<IExerciseFunctions>();
+			_editWorkout = new EditWorkout(_mockExerciseFunctions.Object);
+		}
+
+		[Test]
+		public void TestButton2Click_ExerciseFound_ShowsNewExerciseDialog () {
+			// Arrange
+			var exercise = new Exercise("Test");
+			_mockExerciseFunctions.Setup(x => x.SearchForExercise(It.IsAny<List<Exercise>>(), It.IsAny<string>())).Returns(exercise);
+
+			// Act
+			_editWorkout.button2_Click(null, null);
+
+			// Assert
+			// Here you would assert that ShowNewExerciseDialog was called with the correct parameters.
+			// This might involve making ShowNewExerciseDialog virtual and overriding it in a test-specific subclass, or it might involve some other form of interaction testing.
+		}
+
+		[Test]
+		public void TestDeleteButtonClick_ItemSelected_RemovesExercise () {
+			// Arrange
+			_mockExerciseFunctions.Setup(x => x.RemoveExercise(It.IsAny<List<Exercise>>(), It.IsAny<string>()));
+
+			// Act
+			_editWorkout.DeleteButton_Click(null, null);
+
+			// Assert
+			_mockExerciseFunctions.Verify(x => x.RemoveExercise(It.IsAny<List<Exercise>>(), It.IsAny<string>()), Times.Once);
+		}
 	}
+}
 }
