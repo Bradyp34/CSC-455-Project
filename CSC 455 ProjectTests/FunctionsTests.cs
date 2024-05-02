cb07365c3d08614ed90a23ca61f986d7b82749fb
bradyp34 <brady.petterson@gmail.com>
2024-05-02 12:23:40 -0500

    Added more tests (they finally work yay)

--- CSC 455 ProjectTests/FunctionsTests.cs
+++ CSC 455 ProjectTests/FunctionsTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CSC_455_Project.Tests {
 	[TestClass()]
@@ -26,7 +27,7 @@ namespace CSC_455_Project.Tests {
 		}
 
 		[TestMethod()]
-		public void SearchForWorkoutFailTest() {
+		public void SearchForWorkoutFailTest () {
 			// Arrange
 			var workout1 = new Workout("Workout 1");
 			var workout2 = new Workout("Workout 2");
@@ -58,7 +59,7 @@ namespace CSC_455_Project.Tests {
 		}
 
 		[TestMethod()]
-		public void SearchForExerciseFailTest() {
+		public void SearchForExerciseFailTest () {
 			// Arrange
 			var exercise1 = new Exercise("Exercise 1");
 			var exercise2 = new Exercise("Exercise 2");
@@ -226,7 +227,7 @@ namespace CSC_455_Project.Tests {
 		}
 
 		[TestMethod]
-		public void RemoveExerciseTest() {
+		public void RemoveExerciseTest () {
 			// Arrange
 			var exercise1 = new Exercise("Exercise 1");
 			var exercise2 = new Exercise("Exercise 2");
@@ -244,7 +245,7 @@ namespace CSC_455_Project.Tests {
 		}
 
 		[TestMethod]
-		public void RemoveExerciseTestFail() {
+		public void RemoveExerciseTestFail () {
 			// Arrange
 			var exercise1 = new Exercise("Exercise 1");
 			var exercise2 = new Exercise("Exercise 2");
@@ -259,6 +260,121 @@ namespace CSC_455_Project.Tests {
 			Assert.IsTrue(exercises.Count == 2);
 			Assert.IsTrue(exercises.Contains(exercise1));
 			Assert.IsTrue(exercises.Contains(exercise2));
+		}
+
+		[TestMethod()]
+		public void AddExerciseToWorkoutTest () {
+			// Arrange
+			var workout = new Workout("Test Workout");
+			string exerciseName = "New Exercise";
+			var listBox = new ListBox();
+
+			// Act
+			Functions.AddExerciseToWorkout(workout, exerciseName, listBox);
+
+			// Assert
+			Assert.IsTrue(workout.exercises.Count == 1);
+			Assert.IsTrue(listBox.Items.Contains(exerciseName));
+		}
+
+		[TestMethod()]
+		public void AddExerciseToWorkoutFailTest () {
+			// Arrange
+			var workout = new Workout("Test Workout");
+			string exerciseName = "";
+			var listBox = new ListBox();
+
+			// Act
+			Functions.AddExerciseToWorkout(workout, exerciseName, listBox);
+
+			// Assert
+			Assert.IsTrue(workout.exercises.Count == 0);
+			Assert.IsTrue(listBox.Items.Count == 0);
+		}
+
+		[TestMethod()]
+		public void EditExerciseInWorkoutTest () {
+			// Arrange
+			var workout = new Workout("Test Workout");
+			workout.addExercise(new Exercise("Exercise 1"));
+			var listBox = new ListBox();
+			listBox.Items.Add("Exercise 1");
+			listBox.SelectedItem = "Exercise 1";
+
+			// Act
+			Functions.EditExerciseInWorkout(workout, listBox);
+
+			// Assert
+			// Assuming ShowNewExerciseDialog launches the edit dialog successfully
+			// Assertions would normally be about checking the outcome post-dialog, but dialogs require UI testing frameworks
+		}
+
+		[TestMethod()]
+		public void RemoveExerciseFromWorkoutTest () {
+			// Arrange
+			var workout = new Workout("Test Workout");
+			var exercise = new Exercise("Exercise 1");
+			workout.addExercise(exercise);
+			var listBox = new ListBox();
+			listBox.Items.Add("Exercise 1");
+			listBox.SelectedItem = "Exercise 1";
+
+			// Act
+			Functions.RemoveExerciseFromWorkout(workout, listBox);
+
+			// Assert
+			Assert.IsFalse(workout.exercises.Contains(exercise));
+			Assert.IsTrue(listBox.Items.Count == 0);
+		}
+
+		[TestMethod()]
+		public void PopulateWorkoutsListBoxTest () {
+			// Arrange
+			var workouts = new HashSet<Workout> {
+				new Workout("Workout 1"),
+				new Workout("Workout 2")
+			};
+			var listBox = new ListBox();
+
+			// Act
+			Functions.PopulateWorkoutsListBox(workouts, listBox);
+
+			// Assert
+			Assert.AreEqual(2, listBox.Items.Count);
+			Assert.IsTrue(listBox.Items.Contains("Workout 1"));
+			Assert.IsTrue(listBox.Items.Contains("Workout 2"));
+		}
+
+		[TestMethod()]
+		public void AddNewWorkoutTest () {
+			// Arrange
+			var workouts = new HashSet<Workout>();
+			var listBox = new ListBox();
+			string workoutName = "New Workout";
+
+			// Act
+			Functions.AddNewWorkout(workoutName, workouts, listBox);
+
+			// Assert
+			Assert.IsTrue(workouts.Any(w => w.name == workoutName));
+			Assert.IsTrue(listBox.Items.Contains(workoutName));
+		}
+
+		[TestMethod()]
+		public void DeleteSelectedWorkoutTest () {
+			// Arrange
+			var workout1 = new Workout("Workout 1");
+			var workouts = new HashSet<Workout> { workout1 };
+			var listBox = new ListBox();
+			listBox.Items.Add("Workout 1");
+			listBox.SelectedItem = "Workout 1";
+
+			// Act
+			Functions.DeleteSelectedWorkout(listBox, workouts);
+
+			// Assert
+			Assert.IsFalse(workouts.Contains(workout1));
+			Assert.IsFalse(listBox.Items.Contains("Workout 1"));
 		}
 	}
 }
