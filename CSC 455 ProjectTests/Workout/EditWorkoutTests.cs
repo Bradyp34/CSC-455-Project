fb83f942f93dd9b54110332ba6feaad55977aa97
bradyp34 <brady.petterson@gmail.com>
2024-05-02 11:28:31 -0500

    Revert "fixed test page"
    This reverts commit 13e3afd9c6e79e97283fab25d60454b90982075a.

--- CSC 455 ProjectTests/Workout/EditWorkoutTests.cs
+++ CSC 455 ProjectTests/Workout/EditWorkoutTests.cs
@@ -5,69 +5,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using Moq;
 
 namespace CSC_455_Project_Tests {
-	[TestClass]
+	[TestFixture]
 	public class EditWorkoutTests {
-		private Mock<IExerciseFunctions>? _mockExerciseFunctions;
-		private EditWorkout? _editWorkout;
-		private Workout? _mockWorkout;
+		private Mock<IExerciseFunctions> _mockExerciseFunctions;
+        private EditWorkout _editWorkout;
+        private Workout _mockWorkout;
 
-		[TestInitialize]
-		public void SetUp () {
-			_mockExerciseFunctions = new Mock<IExerciseFunctions>();
-			_mockWorkout = new Workout("Sample Workout");  // Assuming Workout has a constructor that takes a name.
-			_editWorkout = new EditWorkout(_mockWorkout, _mockExerciseFunctions.Object);
-		}
+        [SetUp]
+        public void SetUp () {
+            _mockExerciseFunctions = new Mock<IExerciseFunctions>();
+            _mockWorkout = new Workout("Sample Workout");  // Assuming Workout has a constructor that takes a name.
 
-		[TestMethod]
-		public void TestPopulateExercisesList () {
+            // Correctly instantiate EditWorkout with both the Workout and IExerciseFunctions.
+            _editWorkout = new EditWorkout(_mockWorkout, _mockExerciseFunctions.Object);
+        }
+
+		[Test]
+		public void TestButton2Click_ExerciseFound_ShowsNewExerciseDialog () {
 			// Arrange
-			_mockWorkout.exercises = new HashSet<Exercise> {
-				new Exercise("Exercise 1"),
-				new Exercise("Exercise 2"),
-				new Exercise("Exercise 3")
-			};
+			var exercise = new Exercise("Test");
+			_mockExerciseFunctions.Setup(x => x.SearchForExercise(It.IsAny<List<Exercise>>(), It.IsAny<string>())).Returns(exercise);
 
 			// Act
-			_editWorkout.PopulateExercisesList();
+			_editWorkout.button2_Click(null, null);
 
 			// Assert
-			Assert.AreEqual(3, _editWorkout.listBox1.Items.Count);
+			// Here you would assert that ShowNewExerciseDialog was called with the correct parameters.
+			// This might involve making ShowNewExerciseDialog virtual and overriding it in a test-specific subclass, or it might involve some other form of interaction testing.
 		}
 
-		[TestMethod]
-		public void TestButton1Click () {
+		[Test]
+		public void TestDeleteButtonClick_ItemSelected_RemovesExercise () {
 			// Arrange
-			_editWorkout.NameInput.Text = "New Exercise";
+			_mockExerciseFunctions.Setup(x => x.RemoveExercise(It.IsAny<List<Exercise>>(), It.IsAny<string>()));
 
 			// Act
-			_editWorkout.button1_Click(null, EventArgs.Empty);
+			_editWorkout.DeleteButton_Click(null, null);
 
 			// Assert
-			_mockExerciseFunctions.Verify(m => m.SearchForExercise(It.IsAny<List<Exercise>>(), "New Exercise"), Times.Once);
-			Assert.AreEqual(1, _editWorkout.listBox1.Items.Count);
-		}
-
-		[TestMethod]
-		public void TestButton2Click () {
-			// Arrange
-			_mockWorkout.exercises = new HashSet<Exercise> {
-				new Exercise("Exercise 1"),
-				new Exercise("Exercise 2"),
-				new Exercise("Exercise 3")
-			};
-
-			_mockExerciseFunctions.Setup(m => m.SearchForExercise(It.IsAny<List<Exercise>>(), "Exercise 2")).Returns(new Exercise("Exercise 2"));
-			_editWorkout.listBox1.SelectedItem = "Exercise 2";
-
-			// Act
-			_editWorkout.button2_Click(null, EventArgs.Empty);
-
-			// Assert
-			_mockExerciseFunctions.Verify(m => m.SearchForExercise(It.IsAny<List<Exercise>>(), "Exercise 2"), Times.Once);
-			Assert.AreEqual(3, _editWorkout.listBox1.Items.Count);
+			_mockExerciseFunctions.Verify(x => x.RemoveExercise(It.IsAny<List<Exercise>>(), It.IsAny<string>()), Times.Once);
 		}
 	}
 }
